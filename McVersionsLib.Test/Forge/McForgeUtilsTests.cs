888970a5203018db2689eeb4446dc80b9d0675f0
JeremTech <jeremtech@gmx.fr>
2023-01-05 22:52:21 +0100

    Begin Fabric work

---
+++ McVersionsLib.Test/Forge/McForgeUtilsTests.cs
@@ -0,0 +1,35 @@
+﻿using McVersionsLib.Forge;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace McVersionsLib.Test.Forge
+{
+    /// <summary>
+    /// Test class for McForgeUtils
+    /// </summary>
+    [TestClass]
+    public class McForgeUtilsTests
+    {
+        #region BuildMinecraftForgeMDKDownloadLink
+        [TestMethod]
+        public void BuildMinecraftForgeMDKDownloadLinkTest()
+        {
+            string excepted = "https://files.minecraftforge.net/maven/net/minecraftforge/forge/1.19.2-43.1.47/forge-1.19.2-43.1.47-mdk.zip";
+            Assert.AreEqual(excepted, McForgeUtils.BuildMinecraftForgeMDKDownloadLink("1.19.2-43.1.47"));
+        }
+        #endregion
+
+        #region BuildMinecraftForgeMDKDownloadLink
+        [TestMethod]
+        public void BuildMinecraftForgeInstallerLink()
+        {
+            string excepted = "https://files.minecraftforge.net/maven/net/minecraftforge/forge/1.19.2-43.1.47/forge-1.19.2-43.1.47-installer.jar";
+            Assert.AreEqual(excepted, McForgeUtils.BuildMinecraftForgeInstallerLink("1.19.2-43.1.47"));
+        }
+        #endregion
+    }
+}
