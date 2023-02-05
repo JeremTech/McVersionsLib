833578fe1e954c7ede5866a63475be8115200f6c
JeremTech <jeremtech@gmx.fr>
2023-02-05 21:18:09 +0100

    Add Minecraft versions base

---
+++ McVersionsLib.Test/Minecraft/McDataCollectorTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace McVersionsLib.Test.Minecraft
+{
+    internal class McDataCollectorTests
+    {
+    }
+}
