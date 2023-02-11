047bd9ceca4131d6bec222c27eddc594192b68b0
JeremTech <jeremtech@gmx.fr>
2023-02-11 17:45:24 +0100

    Add last McVersions functions and updated package infos

--- McVersionsLib/Minecraft/McVersions.cs
+++ McVersionsLib/Minecraft/McVersions.cs
@@ -21,7 +21,7 @@ namespace McVersionsLib.Minecraft
         /// Return all available Minecraft versions
         /// </summary>
         /// <param name="forceRetrievingData">Force retrieving data from Mojang Studios servers</param>
-        /// <returns>List of all available Minecraft versions (releases, snapshots, betas and alphas)</returns>
+        /// <returns>List of all available Minecraft versions (releases, snapshots, betas and alphas) ordered by release date (descending)</returns>
         /// <exception cref="WebException">Cannot retrieve Minecraft versions data</exception>
         public static List<string> GetAllAvailableMinecraftVersions(bool forceRetrievingData = false)
         {
@@ -39,7 +39,64 @@ namespace McVersionsLib.Minecraft
                 }
             }
 
-            result.AddRange(MinecraftVersionsData.AllVersionsData.Select(v => v.Version));
+            result.AddRange(MinecraftVersionsData.AllVersionsData.OrderByDescending(v => v.ReleaseDate)
+                                                                 .Select(v => v.Version));
+            return result;
+        }
+
+        /// <summary>
+        /// Return all available Minecraft release versions
+        /// </summary>
+        /// <param name="forceRetrievingData">Force retrieving data from Mojang Studios servers</param>
+        /// <returns>List of all available Minecraft release versions ordered by release date (descending)</returns>
+        /// <exception cref="WebException">Cannot retrieve Minecraft versions data</exception>
+        public static List<string> GetAllAvailableMinecraftReleaseVersions(bool forceRetrievingData = false)
+        {
+            List<string> result = new List<string>();
+
+            if (MinecraftVersionsData == null || forceRetrievingData)
+            {
+                try
+                {
+                    MinecraftVersionsData = McDataCollector.RetrieveAllAvailableMinecraftVersionsData();
+                }
+                catch (WebException e)
+                {
+                    throw e;
+                }
+            }
+
+            result.AddRange(MinecraftVersionsData.AllVersionsData.OrderByDescending(v => v.ReleaseDate)
+                                                                 .Where(v => v.VersionType == McVersionType.Release)
+                                                                 .Select(v => v.Version));
+            return result;
+        }
+
+        /// <summary>
+        /// Return all available Minecraft snapshot versions
+        /// </summary>
+        /// <param name="forceRetrievingData">Force retrieving data from Mojang Studios servers</param>
+        /// <returns>List of all available Minecraft snapshot versions ordered by release date (descending)</returns>
+        /// <exception cref="WebException">Cannot retrieve Minecraft versions data</exception>
+        public static List<string> GetAllAvailableMinecraftSnapshotVersions(bool forceRetrievingData = false)
+        {
+            List<string> result = new List<string>();
+
+            if (MinecraftVersionsData == null || forceRetrievingData)
+            {
+                try
+                {
+                    MinecraftVersionsData = McDataCollector.RetrieveAllAvailableMinecraftVersionsData();
+                }
+                catch (WebException e)
+                {
+                    throw e;
+                }
+            }
+
+            result.AddRange(MinecraftVersionsData.AllVersionsData.OrderByDescending(v => v.ReleaseDate)
+                                                                 .Where(v => v.VersionType == McVersionType.Snapshot)
+                                                                 .Select(v => v.Version));
             return result;
         }
     }
